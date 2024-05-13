77a50f09a24a6de3497fdcca0f606da7d59f465d
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-13 12:05:49 -0700

    debugging

--- neuralnet/train_GoAT.py
+++ neuralnet/train_GoAT.py
@@ -105,10 +105,10 @@ class LitGoAT(L.LightningModule):
         return loss
     
     def validation_step(self, batch, batch_idx):
-        if self.calculate_eval_metrics == False:
+        # if self.calculate_eval_metrics == False:
+        pass
             
-
-        
+             
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.init_lr, weight_decay = 0, amsgrad= True)
