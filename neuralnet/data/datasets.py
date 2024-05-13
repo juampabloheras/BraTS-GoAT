2e2a9afb9ce91f710bda5571ee9042ca0525d56d
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-13 16:17:57 -0700

    debugging

--- neuralnet/data/datasets.py
+++ neuralnet/data/datasets.py
@@ -116,7 +116,7 @@ class LoadDatasetswClusterID(Dataset):
 
         print(f'sampleID, {sampleID}')
         clusterID = self.reverse_mapping.get(sampleID, None) # returns clusterID asssigned to sample if it is assigned, None otherwise.
-        print(f'clusterID, {sampleID}')
+        print(f'clusterID, {clusterID}')
 
         if clusterID is None:
             print(f"Sample {path} not found in any cluster's assignment list.")
