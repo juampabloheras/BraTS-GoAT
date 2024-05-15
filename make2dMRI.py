ad205e313880e50acf11c4d97ffdd5c470e3753d
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-15 15:06:23 -0700

    added make2d images

--- make2dMRI.py
+++ make2dMRI.py
@@ -10,30 +10,36 @@ from torchvision import transforms
 
 
 # Function to make 2D pngs of selected slice of all 3D MRI scans in a directory in pkl format, and save them in an out directory.
-def make2dMRI(in_dir, out_dir, gt_provided = True, slice_no = 64, contrast_no = 0):
-    # files_list = os.listdir(in_dir)
-    # files_list = [os.path.join(in_dir, filename) for filename in files_list]
+def make2dMRI(in_dir, out_dir, gt_provided=True, slice_no=64, contrast_no=0):
+    # Data transforms
+    data_transforms = trans.Compose([
+        trans.CenterCropBySize([128, 192, 128]),
+        trans.NumpyType((np.float32, np.float32, np.float32, np.float32, np.float32)),
+    ])
 
-    data_transforms = transforms.Compose([trans.CenterCropBySize([128,192,128]), 
-                                              trans.NumpyType((np.float32, np.float32,np.float32, np.float32,np.float32)),
-                                              ])
-    
-    dataset = LoadDatasetswClusterID(in_dir, data_transforms, {} , gt_provided=gt_provided, partial_file_names= False) # Loads as (case_info, data, clusterID), where data contains (x1, x2, x3, x4, segmentation)
-    dl = DataLoader(dataset, batch_size=2, num_workers=3)
+    # Load dataset and make dataloader
+    dataset = LoadDatasetswClusterID(in_dir, data_transforms, {}, gt_provided=gt_provided, partial_file_names=False)
+    dl = DataLoader(dataset, batch_size=10, num_workers=3)
 
-    for filename_id, imgs, _ in dl:
-        image = imgs[contrast_no]
+    # Ensure output directory exists
+    os.makedirs(out_dir, exist_ok=True)
 
-        # Plotting
-        plt.figure(figsize=(10, 10), dpi=300)
-        plt.imshow(image.numpy()[0, 0, :, :,slice_no], cmap='gray')   
-        plt.savefig(os.path.join(out_dir, f'{filename_id}.png'))
-        plt.close()
+    # Iterate over DataLoader
+    for batch_idx, (filename_ids, imgs, _) in enumerate(dl):
+        # Iterate over each image in the batch
+        for i in range(len(filename_ids)):
+            filename_id = filename_ids[i]
+            image = imgs[i][contrast_no]
 
-        print(f"Filename ID: {filename_id}")
-        print(f"Filename ID type: {type(filename_id)}")
+            # Plotting
+            plt.figure(figsize=(10, 10), dpi=300)
+            plt.imshow(image.numpy()[0, :, :, slice_no], cmap='gray')
+            plt.savefig(os.path.join(out_dir, f'{filename_id}.png'))
+            plt.close()
 
-        print(f"Plotted figure {filename_id}, saved in {os.path.join(out_dir, f'{filename_id}.png')}!")
+            print(f"Filename ID: {filename_id}")
+            print(f"Filename ID type: {type(filename_id)}")
+            print(f"Plotted figure {filename_id}, saved in {os.path.join(out_dir, f'{filename_id}.png')}!")
 
 
 if __name__ == '__main__':
