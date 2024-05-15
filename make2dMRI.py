a54bd6608c0df85afb30c9cc93dd4ffc698dad5a
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-15 14:32:24 -0700

    added make2d images

--- make2dMRI.py
+++ make2dMRI.py
@@ -4,22 +4,20 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 from neuralnet.data import trans
-
-# # To import from sibling directory
-# parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-# sys.path.append(parent_dir)
-
 from neuralnet.data.datasets import LoadDatasetswClusterID
+from torchvision import transforms
 
 
 
 # Function to make 2D pngs of selected slice of all 3D MRI scans in a directory in pkl format, and save them in an out directory.
 def make2dMRI(in_dir, out_dir, gt_provided = True, slice_no = 64, contrast_no = 0):
     files_list = os.listdir(in_dir)
-    transforms = transforms.Compose([trans.CenterCropBySize([128,192,128]), 
+
+    data_transforms = transforms.Compose([trans.CenterCropBySize([128,192,128]), 
                                               trans.NumpyType((np.float32, np.float32,np.float32, np.float32,np.float32)),
                                               ])
-    dataset = LoadDatasetswClusterID(in_dir, transforms, {} , gt_provided=gt_provided, partial_file_names= files_list) # Loads as (case_info, data, clusterID), where data contains (x1, x2, x3, x4, segmentation)
+    
+    dataset = LoadDatasetswClusterID(in_dir, data_transforms, {} , gt_provided=gt_provided, partial_file_names= files_list) # Loads as (case_info, data, clusterID), where data contains (x1, x2, x3, x4, segmentation)
     dl = DataLoader(dataset, batch_size=1, num_workers=3)
 
     for filename_id, imgs, _ in dl:
