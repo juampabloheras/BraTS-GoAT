960360e026969a0bf69bc427804f08a70dd977ea
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-15 14:17:20 -0700

    added make2d images

--- clustering/make2dMRI.py
+++ clustering/make2dMRI.py
@@ -2,6 +2,13 @@ import os
 from neuralnet.data.datasets import LoadDatasetswClusterID
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import sys
+
+# To import from sibling directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from neuralnet.data.datasets import LoadDatasetswClusterID
+
 
 # Function to make 2D pngs of selected slice of all 3D MRI scans in a directory in pkl format, and save them in an out directory.
 def make2dMRI(in_dir, out_dir, gt_provided = True, slice_no = 64, contrast_no = 0):
