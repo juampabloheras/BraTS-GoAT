58b34e81d0c2541c2a7d309babf1b977e7a81769
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-15 13:58:02 -0700

    added make2d images

--- neuralnet/data/datasets.py
+++ neuralnet/data/datasets.py
@@ -158,7 +158,8 @@ class LoadDatasetswClusterID(Dataset):
 
         # Get case_id from filename - ADDED by Ethan 17 July 2023
         filename = path.split('/')[-1]
-        case_info = tuple(filename.split('.')[0].split('-')[2:4]) #(case_id, timepoint)
+        case_info = filename.split('.')[0] # e.g. from BraTS-GoAT-00000.pkl will produce BraTS-GoAT-00000
+        # case_info = tuple(filename.split('.')[0].split('-')[2:4]) #(case_id, timepoint)
 
         if self.gt_provided:
             data = x1, x2, x3, x4, y1
