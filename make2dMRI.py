feee7e7d6f15de457510efb62a73e9a46db2d54e
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-17 14:50:24 -0700

    added make2d images

--- make2dMRI.py
+++ make2dMRI.py
@@ -37,7 +37,8 @@ def make2dMRI(in_dir, out_dir, gt_provided=True, slice_no=64, contrast_no=0):
             print(f'Type Imags: {type(imgs)}')
             print(f'Shape Imgs: {np.shape(np.array(imgs))}')
 
-            slice = imgs[contrast_no,i,0, :, :, slice_no].numpy()
+
+            slice = imgs.numpy()[contrast_no,i,0, :, :, slice_no]
             # slice imagege.numpy()[contrast_no,0, :, :, slice_no]
 
 
