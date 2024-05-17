8875fba3dcdec4037c0be990efaa989b0e18d1e9
Juampablo Heras Rivera <96499961+juampabloheras@users.noreply.github.com>
2024-05-17 14:27:30 -0700

    added make2d images

--- make2dMRI.py
+++ make2dMRI.py
@@ -51,8 +51,10 @@ def make2dMRI(in_dir, out_dir, gt_provided=True, slice_no=64, contrast_no=0):
                 continue
             # Plotting
             plt.figure(figsize=(10, 10), dpi=300)
-            print(f'Type Image: {type(image)}')
-            print(f'Shape Image: {np.shape(image)}')
+            print(f'Type Image: {type(image.numpy())}')
+            print(f'Shape Image: {np.shape(image.numpy())}')
+            print(f'Slice shape: {np.shape(image.numpy()[contrast_no, :, :, slice_no])}')
+
 
             slice = image.numpy()[contrast_no, :, :, slice_no]
             plt.imshow(slice, cmap='gray')
